90ea332cde9eb25b623e64595a5c5dc4ad386a51
Gwendolyn Goetz <gwendolyngoetz@users.noreply.github.com>
2022-11-05 23:05:57 -0700

    Adding sample i3 workspace loop

--- cmd/i3-aide/main.go
+++ cmd/i3-aide/main.go
@@ -1,7 +1,22 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"log"
+
+	"go.i3wm.org/i3/v4"
+)
 
 func main() {
+	workspaces, err := i3.GetWorkspaces()
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	for i, s := range workspaces {
+		fmt.Println(i, s.Name)
+	}
+
+	fmt.Println("")
 	fmt.Println("done")
 }
